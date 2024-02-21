31f2b0b44ead18f7e28c345cbc85cb873378c855
Andrés Tello <andrest911@gmail.com>
2024-02-21 11:20:41 -0300

    desplegando

--- practica2/src/socket.js
+++ practica2/src/socket.js
@@ -25,7 +25,7 @@ console.log("aqui te tengo tu variable especial", varEspecial)
 
         if(varEspecial){
 
-            socketClient.emit('serverms', 'Bienvenido nuevo cliente 😃')
+            socketClient.emit('serverms', 'Bienvenido nuevo cliente 😃😀')
         }else{
             socketClient.emit('servermsg', 'desaparecer')
         }
